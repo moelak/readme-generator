cdcb221bac1e4704bc39b957c6921f06e0f68e39
Moe <66816952+moelak@users.noreply.github.com>
2020-09-01 23:07:35 -0700

    updated the License

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -5,8 +5,26 @@ function generateMarkdown(data) {
 
   let newItems = items.join(' ');
 
+  let licenseLink = [];
+
+  if (data.license === 'BSD') {
+    licenseLink.push(
+      'https://en.wikipedia.org/wiki/BSD_licenses#:~:text=BSD%20licenses%20are%20a%20family,and%20distribution%20of%20covered%20software.&text=The%20original%20BSD%20license%20was,a%20Unix%2Dlike%20operating%20system.'
+    );
+  } else if (data.license === 'MIT') {
+    licenseLink.push('https://en.wikipedia.org/wiki/MIT_License');
+  } else {
+    licenseLink.push(
+      'https://en.wikipedia.org/wiki/GNU_General_Public_License'
+    );
+  }
+
   return `
 
+  [![Generic badge](https://img.shields.io/badge/License-${
+    data.license
+  }-<COLOR>.svg)](${licenseLink.toString()})
+  
   # ${data.title}\n
   
   ### **Description:** ${data.description}\n
@@ -23,41 +41,37 @@ function generateMarkdown(data) {
  * [Questions](#questions)
  
  ## Installation
----
+
 * ${data.firstItem}
 ${newItems}
 
 ## Usage
----
+
 
 ${data.usage}
 
 ## Credits
----
+
 ${data.credit}
 
 ## License
----
-#### License: ${data.license}
 
-## Badges
----
-[![Generic badge](https://img.shields.io/badge/${data.badges}-${data.badgeValue}%-<COLOR>.svg)](https://shields.io/)
+#### License: ${data.license}
 
 
 ## Contributing
----
+
 ${data.contributing}
 
 ## Tests
----
+
 
 \`\`\`
 ${data.tests}
 \`\`\`
 
 ## Questions
----
+
 \`\`\`
 ${data.questions}
 \`\`\`
